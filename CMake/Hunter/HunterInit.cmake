f5ae33b96ca97e9f4b15b36caae9415a167f51c1
Kevin Scroggins <nitro404@gmail.com>
2026-01-29 22:44:25 -0500

    WIP CMake support

---
+++ CMake/Hunter/HunterInit.cmake
@@ -0,0 +1,9 @@
+include_guard()
+
+include(${ROOT_DIRECTORY}/CMake/Hunter/HunterGate.cmake)
+
+HunterGate(
+	URL "https://github.com/Telefrag-Software/hunter/archive/v0.26.6.3.tar.gz"
+	SHA1 "40bdfd3a9e55ee01270d4bd85cdca7656fdaadfe"
+	FILEPATH "${ROOT_DIRECTORY}/CMake/Hunter/HunterConfig.cmake"
+)
